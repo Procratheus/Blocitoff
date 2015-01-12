2cf31c2941bb5ef4fa958c59e0b049b9b82041b5
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-01-12 23:39:03 +0000

    Updated Gemfile for deploymnet to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+    def blocitoff
+      render text: "<h>Blocitoff</h><p>A self destructing ToDo list</p>"
+    end
 end
