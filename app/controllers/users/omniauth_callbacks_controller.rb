463e1538d1116b06b1f682b31cca592dfd1e0835
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-01-21 17:24:50 +0000

    debugged omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => authentication
+      sign_in_and_redirect @user, :event => :authentication
     else
       session['devise.twitter_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.name + '@twitter.com'
+      user.email = auth.uid + '@twitter.com'
       user.password = Devise.friendly_token[0,20]
+      user.provider = auth.provider
+      user.uid = auth.uid
     end
   end
     
