713945f8fc1d78cc37f5aea25d927e3823e68b7c
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-01-24 22:30:02 +0000

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
- devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+ devise_for :users, :controllers => { :omniauth_callbacks => 'omniauth_callbacks' }
  resources :users, only: [:update]
  resources :lists, except: [:index]
  root "welcome#index"
