f79c8bc1aec337aa6934f298d96cf4245105e976
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-01-24 12:22:20 +0000

    Fixed Omniauth Twitter

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,9 @@
 class ListsController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user! # user must be signed in before any lists controller action can be accessed
 
   def show
-    @list = current_user.list
+    @list = List.find(params[:id])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:twitter]
 
   has_one :list
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.uid + '@twitter.com'
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token
       user.provider = auth.provider
       user.uid = auth.uid
     end
