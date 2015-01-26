a69bd82d848f45d6c38f2cf9388d02b6951135a3
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-01-26 21:45:16 +0000

    custom message

--- lib/list_one_validator.rb
+++
@@ -1,8 +0,0 @@
-class ListOneValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value.user_id 
-      value.errors[attribute] << (options[:message] || "There can be only one! 'Highlander proceeds to chop the Kurgans head off.'")
-    end
-  end
-end
-
